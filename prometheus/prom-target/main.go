8c68a2cdbec3729b9f32a71a7a3d7680486f4973
tyltr <tylitianrui@126.com>
2024-09-26 00:46:45 +0800

    代码

--- prometheus/prom-target/main.go
+++ prometheus/prom-target/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -12,115 +14,99 @@ import (
 )
 
 var (
-	// counter  案例
+	//HTTP请求总数
 	requestTotal = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "tyltr_request_total", //  counter命名  xxxx__total
-			Help: "请求总数",
+			Name: "tyltr_request_total", //  counter
+			Help: "HTTP请求总数",
 		},
 		[]string{"method", "path", "code"}, // labels
 	)
 
 	avgLoad = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Name: "tyltr_sys_avg_load",
+			Name: "tyltr_sys_avg_load", //  Gauge
 			Help: "系统负载",
 		},
-		[]string{"min"},
+		[]string{"min"}, // labels
+	)
+	respDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "tyltr_request_duration",
+			Help:    "响应时间",
+			Buckets: []float64{2, 5, 8},
+		},
+		[]string{"path"},
 	)
 )
 
+// 初始化prometheus收集器
 func InitPrometheusCollector() {
 	prometheus.MustRegister(requestTotal)
 	prometheus.MustRegister(avgLoad)
+	prometheus.MustRegister(respDuration)
 }
 
-func main() {
-	InitPrometheusCollector()
-	NewApiServer()
-}
-
-type Wrapper struct {
-}
-
-func (h *Wrapper) printReq(r *http.Request) {
-	fmt.Println("\n\r\n\r\n\r")
-	fmt.Println("method:", r.Method)
-	fmt.Println("url:", r.Host+r.URL.String())
-	// 打印header
-	if len(r.Header) > 0 {
-		fmt.Println("-----header start------")
-	}
-	for k, v := range r.Header {
-		if len(v) == 1 {
-			fmt.Println(k, "=", v[0])
-		} else {
-			fmt.Println(k, "=", v)
-		}
-
-	}
-	if len(r.Header) > 0 {
-		fmt.Println("-----header end------")
-	}
-
-	// 打印query
-	q := r.URL.Query()
-	if len(q) > 0 {
-		fmt.Println("-----Query start------")
-	}
-
-	for k, v := range q {
-		fmt.Println("k:", k, "val:", v)
-	}
-	if len(q) > 0 {
-		fmt.Println("-----Query end------")
-	}
-}
-
-func (h *Wrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.printReq(r) //  打印请求参数
-	promhttp.Handler().ServeHTTP(w, r)
-}
-
-func NewApiServer() error {
-	go func() {
-		http.Handle("/metrics", &Wrapper{})
-		http.ListenAndServe(":9100", nil)
-		// curl  http://127.0.0.1:9100/metrics
-	}()
-	go func() {
-		ticker := time.NewTicker(time.Second)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				CpuUseGauge()
-			}
-		}
-	}()
-
-	app := gin.Default()
-	app.Use(PrometheusRequestTotalMiddleWare)
-
-	app.GET("/ping/*name", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+// 对外暴漏/metrics接口
+func RunPrometheusMetricsApi() {
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		promhttp.Handler().ServeHTTP(w, r)
 	})
-	return app.Run(":8520")
-
+	// http://127.0.0.1:9100/metrics
+	http.ListenAndServe(":9100", nil)
 }
 
+// 模拟业务处理延迟
+func HttpBizLatency() {
+	latency := time.Millisecond * time.Duration(rand.Intn(10))
+	time.Sleep(latency)
+}
+
+// 统计 http请求
 func PrometheusRequestTotalMiddleWare(c *gin.Context) {
 	meth := c.Request.Method
 	path := c.Request.URL.Path
 	c.Next()
 	code := c.Writer.Status()
 	requestTotal.WithLabelValues(meth, path, fmt.Sprintf("%d", code)).Add(1)
+}
+
+// 统计 http请求
+func PrometheusRequestLatency(c *gin.Context) {
+	start := time.Now().UnixNano()
+	path := c.Request.URL.Path
+	c.Next()
+	duration := (time.Now().UnixNano() - start) / (1000 * 1000)
+
+	respDuration.WithLabelValues(path).Observe(float64(duration))
 
 }
 
-func CpuUseGauge() {
+// http服务，模拟对外正常的业务服务
+func RunHttpApiServer() error {
+	app := gin.Default()
+	// 统计 http请求
+	app.Use(PrometheusRequestLatency, PrometheusRequestTotalMiddleWare)
+
+	app.GET("/ping/:id", func(c *gin.Context) {
+		HttpBizLatency()
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"id": id,
+		})
+	})
+	return app.Run(":8520")
+}
+
+// 获取系统负载
+func AvgLoad() {
 	loadInfo, err := load.Avg()
 	if err != nil {
 		return
@@ -128,6 +114,26 @@ func CpuUseGauge() {
 	avgLoad.WithLabelValues("1").Set(loadInfo.Load1)
 	avgLoad.WithLabelValues("5").Set(loadInfo.Load5)
 	avgLoad.WithLabelValues("15").Set(loadInfo.Load15)
-
 }
 
+// 每秒一次收集负载
+func RunAvgLoadCollector() {
+	go func() {
+		ticker := time.NewTicker(time.Second)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				AvgLoad()
+			}
+		}
+	}()
+}
+
+func main() {
+	InitPrometheusCollector()
+	go RunPrometheusMetricsApi()
+	go RunAvgLoadCollector()
+
+	RunHttpApiServer()
+}
